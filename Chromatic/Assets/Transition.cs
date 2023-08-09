97081123a264dc2c7169634dafd0c30a84c240f0
Jacques-afk <s10243606@connect.np.edu.sg>
2023-08-09 15:19:07 +0800

    mainly removed a package

--- Chromatic/Assets/Transition.cs
+++ Chromatic/Assets/Transition.cs
@@ -6,15 +6,21 @@ using UnityEngine.VFX;
 public class Transition : MonoBehaviour
 {
     public VisualEffect fireworks;
+    public ParticleSystem confetti;
 
     void Start(){
         fireworks.Stop();
+        confetti.Stop();
     }
 
     public void endDecrease(){
+        confetti.Play();
         StartCoroutine(fireworksStop());
     }
     
+
+
+
     IEnumerator fireworksStop(){
         fireworks.Play();
         yield return new WaitForSeconds(2f);
